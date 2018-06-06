aadef71ab86f716b4011715e29acb8056d93cad9
Kartik Kumar <me@kartikkumar.com>
2018-06-06 15:44:28 +0200

    Remove Eigen from project.

--- Dependencies.cmake
+++ Dependencies.cmake
@@ -1,4 +1,4 @@
-# Copyright (c) 2017 Kartik Kumar (me@kartikkumar.com)
+# Copyright (c) 2018 Kartik Kumar (me@kartikkumar.com)
 # Distributed under the MIT License.
 # See accompanying file LICENSE.md or copy at http://opensource.org/licenses/MIT
 
@@ -7,45 +7,7 @@ include(ExternalProject)
 
 # -------------------------------
 
-# sml: https://github.com/openastro/sml
-
-if(NOT BUILD_DEPENDENCIES)
-  find_package(sml)
-endif(NOT BUILD_DEPENDENCIES)
-
-if(NOT SML_FOUND)
-  message(STATUS "sml will be downloaded when ${CMAKE_PROJECT_NAME} is built")
-  ExternalProject_Add(sml-lib
-    PREFIX ${EXTERNAL_PATH}/sml
-    #--Download step--------------
-    URL https://github.com/openastro/sml/archive/master.zip
-    TIMEOUT 30
-    #--Update/Patch step----------
-    UPDATE_COMMAND ""
-    PATCH_COMMAND ""
-    #--Configure step-------------
-    CONFIGURE_COMMAND ""
-    #--Build step-----------------
-    BUILD_COMMAND ""
-    #--Install step---------------
-    INSTALL_COMMAND ""
-    #--Output logging-------------
-    LOG_DOWNLOAD ON
-  )
-  ExternalProject_Get_Property(sml-lib source_dir)
-  set(SML_INCLUDE_DIRS ${source_dir}/include CACHE INTERNAL "Path to include folder for sml")
-endif(NOT SML_FOUND)
-
-if(NOT APPLE)
-  include_directories(SYSTEM AFTER "${SML_INCLUDE_DIRS}")
-else(APPLE)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem \"${SML_INCLUDE_DIRS}\"")
-endif(NOT APPLE)
-
-# -------------------------------
-
 # Catch: https://github.com/philsquared/Catch
-# Eigen: http://eigen.tuxfamily.org
 
 if(BUILD_TESTS)
   if(NOT BUILD_DEPENDENCIES)
@@ -80,41 +42,6 @@ if(BUILD_TESTS)
   else(APPLE)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem \"${CATCH_INCLUDE_DIRS}\"")
   endif(NOT APPLE)
-
-  if(BUILD_TESTS_WITH_EIGEN)
-    if(NOT BUILD_DEPENDENCIES)
-      find_package(Eigen3)
-    endif(NOT BUILD_DEPENDENCIES)
-
-    if(NOT EIGEN3_FOUND)
-      message(STATUS "Eigen will be downloaded when ${CMAKE_PROJECT_NAME} is built")
-      ExternalProject_Add(eigen-lib
-        PREFIX ${EXTERNAL_PATH}/Eigen
-        #--Download step--------------
-        URL https://bitbucket.org/eigen/eigen/get/3.2.9.zip
-        TIMEOUT 60
-        #--Update/Patch step----------
-        UPDATE_COMMAND ""
-        PATCH_COMMAND ""
-        #--Configure step-------------
-        CONFIGURE_COMMAND ""
-        #--Build step-----------------
-        BUILD_COMMAND ""
-        #--Install step---------------
-        INSTALL_COMMAND ""
-        #--Output logging-------------
-        LOG_DOWNLOAD ON
-      )
-      ExternalProject_Get_Property(eigen-lib source_dir)
-      set(EIGEN3_INCLUDE_DIR ${source_dir} CACHE INTERNAL "Path to include folder for Eigen")
-    endif(NOT EIGEN3_FOUND)
-
-    if(NOT APPLE)
-      include_directories(SYSTEM AFTER "${EIGEN3_INCLUDE_DIR}")
-    else(APPLE)
-      set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem \"${EIGEN3_INCLUDE_DIR}\"")
-    endif(NOT APPLE)
-  endif(BUILD_TESTS_WITH_EIGEN)
 endif(BUILD_TESTS)
 
 # -------------------------------
